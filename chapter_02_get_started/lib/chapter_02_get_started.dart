4f74499f452e404ccdc16b4a29693249387a6d8d
Viktor Lidholt <viktorlidholt@gmail.com>
2023-12-30 18:04:07 +0100

    fix: Correctly renames slides.

--- chapter_02_get_started/lib/chapter_02_get_started.dart
+++ chapter_02_get_started/lib/chapter_02_get_started.dart
@@ -1,8 +1,8 @@
 import 'package:chapter_02_get_started/chapter_02_get_started.dart';
 
-export 'src/slides/slide_04_title_get_started.dart';
-export 'src/slides/slide_05_installation.dart';
-export 'src/slides/slide_06_create_project.dart';
+export 'src/slides/slide_00_title_get_started.dart';
+export 'src/slides/slide_01_installation.dart';
+export 'src/slides/slide_02_create_project.dart';
 
 final slidesGetStarted = [
   slideTitleGetStarted,
